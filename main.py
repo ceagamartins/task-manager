0dd58a051d4bbc0a2450ff0f9001d3ff116beeb9
Christian Martins <martins.christian@live.com>
2026-02-18 03:01:16 -0300

    Implementando front end com Bootstrap

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 from database import engine
 import models
 from routers import tarefas
 
+# Cria as tabelas no banco automaticamente
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Configuração da pasta de templates
+templates = Jinja2Templates(directory="templates")
+
+# Inclui as rotas de tarefas (CRUD)
 app.include_router(tarefas.router)
 
-@app.get("/")
-def read_root():
-    return{"message": "API com banco funcionando"}
+# Página principal (HTML)
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# Rota simples de status
+@app.get("/status")
+def status():
+    return {"status": "API online"}
